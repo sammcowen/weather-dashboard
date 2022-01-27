d36ed7cd6505cd61c01dcadd3b2dea751c926ae1
HBK <sammcowen@gmail.com>
2022-01-27 10:09:18 -0800

    created functions to fetch weather and coords based on city user input

--- assets/js/app.js
+++ assets/js/app.js
@@ -7,24 +7,44 @@ const rootApiUrl = 'https://api.openweathermap.org';
 const myApiKey = '94bb7586d2b9a5b650867004d68a2e5d';
 
 //functions
-function fetchLoc(city) {
-    var apiUrl = `${rootApiUrl}/geo/1.0/direct?q=${city}&limit=5&appid=${myApiKey}`;
+function fetchCoord(city) {
+    let apiUrl = `${rootApiUrl}/geo/1.0/direct?q=${city}&limit=1&appid=${myApiKey}`;
     fetch(apiUrl)
     .then(function(response){
             return response.json();
     })
     .then(function(data){
-        console.log('data'. data);
+        console.log('data', data);
+        const lat = data[0].lat;
+        const lon = data[0].lon;
+        console.log('lat',data[0].lat);
+        console.log('lon', data[0].lon);
+
+        fetchWeather();
+    
+
+    })
+
+};
+function fetchWeather() {
+    let apiUrl = `${rootApiUrl}/data/2.5/onecall?lat=${lat}&lon=${lon}&appid=${myApiKey}`;
+    fetch(apiUrl)
+    .then (function(response) {
+        return response.json();
+    })
+    .then(function(data) {
+        console.log(data)
     })
 
 }
 
 
 function search() {
-    const city = inputEl.value.trim();
+    var city = inputEl.value.trim();
     console.log('city', city);
-        fetchLoc(city);
-}
+        fetchCoord(city);
+      
+};
 
 // add event listeners
 buttonEl.addEventListener('click', search);
