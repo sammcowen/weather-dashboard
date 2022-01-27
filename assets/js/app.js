b7305493cfa4dc974655e97acc7b5470497b8a44
HBK <sammcowen@gmail.com>
2022-01-27 12:50:52 -0800

    fetch weather works. renders current day conditions to page

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,10 +1,14 @@
 // element references
 const buttonEl = document.querySelector(".btn");
 const inputEl = document.querySelector('#input');
+const currentCity = document.querySelector('#currentCity');
+
 
 // variables
 const rootApiUrl = 'https://api.openweathermap.org';
 const myApiKey = '94bb7586d2b9a5b650867004d68a2e5d';
+const currentDate = moment().format('ll');
+let searchHistoryList =[];
 
 //functions
 function fetchCoord(city) {
@@ -20,29 +24,49 @@ function fetchCoord(city) {
         console.log('lat',data[0].lat);
         console.log('lon', data[0].lon);
 
-        fetchWeather();
+        fetchWeather(lat,lon);
     
 
     })
 
 };
-function fetchWeather() {
+function fetchWeather(lat, lon) {
     let apiUrl = `${rootApiUrl}/data/2.5/onecall?lat=${lat}&lon=${lon}&appid=${myApiKey}`;
     fetch(apiUrl)
     .then (function(response) {
         return response.json();
     })
     .then(function(data) {
-        console.log(data)
+        console.log(data);
+        const temp =data.current.temp;
+        const humid = data.current.humidity;
+        const windSpeed = data.current.wind_speed;
+        const uvi = data.current.uvi;
+        $("#li1").text("Temp: "+ temp);
+        $("#li2").text("Humidity: "+ humid);
+        $("#li3").text("Wind Speed: "+ windSpeed);
+        $("#li4").text("UVI: "+ uvi);
     })
-
+    
+//renderWeather();
 }
 
+// function renderWeather(temp, humid, windSpeed, uvi) {
+    
 
+// }
+
+//takes user input and displays 
 function search() {
     var city = inputEl.value.trim();
-    console.log('city', city);
+    //console.log('city', city);
         fetchCoord(city);
+        $("#currentCity").text(city);
+        $("#currentDate").text(currentDate);
+        localStorage.setItem("city", JSON.stringify(searchHistoryList));
+        console.log(searchHistoryList);
+        
+
       
 };
 
